3e8e895810f3a9a6c658bf6a5a27980a9099527c
Julien Maupetit <julien@maupetit.net>
2012-04-25 16:02:46 +0200

    First draft for this module

---
+++ france_express/fixtures/gen_fixtures.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import sys,re
+from django.template.defaultfilters import slugify
+from decimal import Decimal as D
+
+FRANCE_EXPRESS_THRESHOLD_WEIGHT = 100
+
+def main():
+    """Quick and dirty script to generate fixtures (initial data) from france express flat file
+    """
+    
+    if len(sys.argv) != 2:
+        print "Usage: %s rate_file.txt" % sys.argv[0]
+        sys.exit(2)
+        
+    ratefile = sys.argv[1]
+    f = open(ratefile)
+    data = f.readlines()
+    json_p = '  {"pk": %d, "model": "france_express.%s", "fields": %s},'
+    offer_p = re.compile(u'^;;(.*)$')
+    zone_p = re.compile(u'^::Zone::(.*)$')
+    weights_p = re.compile(u'^::Weights$')
+    rates_p = re.compile(u'^::Rates$')
+    
+    offer_id = zone_id = dept_id = rate_id = 0
+
+    print "["
+    
+    for i in range(0,len(data)):
+        l = data[i].strip()
+        
+        # Skip empty lines or commentaries
+        if not len(l) or l[0] == "#":
+            continue
+
+        # Offer
+        if offer_p.match(l):
+            offer_name = offer_p.match(l).group(1).strip()
+            offer_slug = slugify(offer_name)
+            offer_id += 1
+            zones = []
+            out = json_p % ( offer_id, "offer", { "name":offer_name })
+            print out
+            
+        # Zone
+        elif zone_p.match(l):
+            zone_name = u'%s-zone-%s' % (offer_slug,zone_p.match(l).group(1).strip())
+            zone_id += 1
+            zones.append(zone_id)
+            out = json_p % ( zone_id, "zone", { "name":zone_name, "offer":offer_id })
+            print out
+
+            # Department
+            for dept in [ int(d) for d in data[i+1].split() ]:
+                dept_id += 1
+                out = json_p % ( dept_id, "department", { "number":dept, "zone":zone_id })
+                print out
+                
+        # Weights
+        elif weights_p.match(l):
+            # Ignore the last weight that is used for weights > 100kg
+            weights = [ D(w) for w in data[i+1].split() ][:-1]
+
+        # Rates
+        elif rates_p.match(l):
+            for z in range(len(zones)):
+                prices  = [ D(p) for p in data[i+z+1].split()[:-1] ]
+                t_price = D(data[i+z+1].split()[-1])
+                # Consider also >100kg prices
+                for w in range(0,40):
+                    for r in range(len(weights)):                                        
+                        rate_id += 1
+                        out = json_p % ( rate_id, "rate", { "weight":"%.2f"%(w*FRANCE_EXPRESS_THRESHOLD_WEIGHT+weights[r]), "price":"%.2f"%(w*t_price+prices[r]), "zone":zones[z] })
+                        print out
+    print "]"
+    f.close()
+    
+
+if __name__ == '__main__':
+	main()
---
+++ france_express/models.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 *-*
+
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext_lazy as _
+from decimal import Decimal
+import types
+from france_express.config import *
+
+class Offer(models.Model):
+    
+    name = models.CharField(_("France Express offer name"), max_length=50)
+
+class Zone(models.Model):
+
+    name = models.CharField(_("Zone name for the related offer"), max_length=255)
+    offer = models.ForeignKey(Offer)
+
+    def __repr__(self):
+        return self.name
+
+class Department(models.Model):
+    
+    number = models.Integer(max_length=3)
+    zone = models.ForeignKey(Zone)
+
+class Rate(models.Model):
+    
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    zone = models.ForeignKey(Zone)
+
+    def __repr__(self):
+        return "<Rate: max %.2fkg (%s) %.2f EUR>" % (self.weight, self.zone, self.price)
+    
+    def get_rate(department_number, weight):
+        
+        if type(department_number) != types.IntType:
+            raise TypeError, "Country must be a string, not %s" % type(country)
+        
+        if type(weight) != types.FloatType and type(weight) != types.IntType:
+            raise TypeError, "Weight must be a float"
+        
+        if weight < 0 or weight > FRANCE_EXPRESS_MAX_WEIGHT:
+            raise ValueError, "FranceExpress only supports 0 < weight < %d" % FRANCE_EXPRESS_MAX_WEIGHT
+        
+        rs = []
+        for zone in [ d.zone for d in Department.objects.filter(number=department_number) ]:
+            w = Rate.objects.filter(weight__gte=Decimal(str(weight)), zone=zone)[0].weight
+            rs += Rate.objects.get(weight=w, zone=zone)
+            
+        return sorted(rs, key=lambda rate: rate.price)
+
+    class Meta:
+        unique_together = (('weight', 'price', 'zone'))
